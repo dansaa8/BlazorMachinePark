0a47eb97f8938be5441059271aadfcdc0fc50935
dansaa8 <dansaa8@protonmail.com>
2024-12-23 12:21:14 +0100

    Add 3 Entities that will act as the base for machines. (Machine, MachineType, Location). Where Machine represents a unique Machine and MachineType and Location serve as fixed values for a machine.

---
+++ BlazorMachinePark.Shared/Domain/Location.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorMachinePark.Shared.Domain
+{
+    public class Location
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Country { get; set; }
+
+        // Navigation property for one-to-many relationship 
+        public ICollection<Machine> Machines { get; set; }
+    }
+}
---
+++ BlazorMachinePark.Shared/Domain/Machine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorMachinePark.Shared.Domain
+{
+    public class Machine
+    {
+        [Key]
+        public Guid Id { get; set; } // GUID as pk
+        public bool IsRunning { get; set; }
+
+        public Location Location { get; set; } // Nav-prop
+        public int LocationId { get; set; }
+
+        public MachineType MachineType { get; set; } // Nav-prop
+        public int MachineTypeId { get; set; }
+
+        public Machine()
+        {
+            Id = Guid.NewGuid(); // Automatically generate GUID when creating a new machine.
+        }
+    }
+}
